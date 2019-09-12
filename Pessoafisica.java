a414579a5e08f023dd6a11aafe21fcbf7f949d49
Lívia Henrique <49006944+liviahenrique100@users.noreply.github.com>
2019-09-12 10:17:12 -0300

    códigos aplicando herança

---
+++ Pessoafisica.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.miguel.pessoa;
+
+/**
+ *
+ * @author aluno
+ */
+public class Pessoafisica extends Pessoa {
+
+	private String cpf;
+
+	public String getRg() {
+		return cpf;
+	}
+
+	public void setRg(String rg) {
+		this.cpf = cpf;
+	}
+	
+}
